ee904a85afb5a5e79a0fd9891e389365cb2e28da
PyYoshi <myoshi321go@gmail.com>
2014-07-10 15:38:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# BUILD
+
+```bash
+$ git clone --recursive https://github.com/PyYoshi/android_kernel_huawei_hw01e_buildtools.git
+$ cd android_kernel_huawei_hw01e_buildtools
+$ ./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   platform='darwin'
+fi
+
+#
+if [ "$platform" == 'linux' ]; then
+    export CCOMPILER=$PWD/bin_linuxx86/arm-eabi-4.4.3/bin/arm-eabi-
+    export MKBOOTIMG=$PWD/bin_linuxx86/mkbootimg
+elif [ "$platform" == 'darwin' ]; then
+    export CCOMPILER=$PWD/bin_darwinx86/arm-eabi-4.4.3/bin/arm-eabi-
+    export MKBOOTIMG=$PWD/bin_darwinx86/mkbootimg
+else
+    echo "Not supported platform!"
+    exit 1
+fi
+
+#
+rm -rf new-ramdisk.cpio.gz
+rm -rf new_boot.img
+
+#
+if [ ! -e "kernel" ]; then
+    git submodule update --init
+fi
+cd kernel
+cp -f arch/arm/configs/hw01e_defconfig .config
+make ARCH=arm CROSS_COMPILE=$CCOMPILER -j4
+cd ..
+if [ -e "./kernel/arch/arm/boot/zImage" ]; then
+    cd boot_ramdisk
+    find . | cpio -o -H newc | gzip > ../new-ramdisk.cpio.gz
+    cd ..
+    $MKBOOTIMG --kernel ./kernel/arch/arm/boot/zImage  --ramdisk ./new-ramdisk.cpio.gz --cmdline "androidboot.hardware=huawei user_debug=31 kgsl.mmutype=gpummu" --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x1400000 -o new_boot.img
+else
+    echo "カーネルのビルドが失敗してるかもしれないよ"
+    echo "kernel/arch/arm/boot/zImageが存在するか確認してちょうだい"
+    exit 1
+fi
+
+exit 0
